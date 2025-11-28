ebb9ad99808a7effc1cafd53a826237b73f35102
100-ANITA <cse24-032@thuto.bac.ac.bw>
2025-11-28 08:00:57 +0200

    Delete src/bank directory

--- src/bank/InterestBearing.java
+++
@@ -1,9 +0,0 @@
-package com.elitebank.model;
-
-public interface InterestBearing {
-    double calculateMonthlyInterest(); // returns interest amount
-
-    default void applyMonthlyInterest() {
-        // default implementation (optional override)
-    }
-}
